0dac9880d069692764ba00470209ab5faa835ba2
Halil  Süzer <halilibrahimsuzer@gmail.com>
2023-11-05 20:12:09 +0300

    asp.net core 5 mini proje #6 ders bitiş commit’i

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -3,15 +3,12 @@ namespace EntityLayer.Concrete
 {
 	public class About
 	{
-		public About()
-		{
-		}
 		public int AboutID { get; set; }
-		public int Title { get; set; }
-		public string Description { get; set; }
-		public string Image1 { get; set; }
-		public string Title2 { get; set; }
-		public string Description2 { get; set; }
+		public int? Title { get; set; }
+		public string? Description { get; set; }
+		public string? Image1 { get; set; }
+		public string? Title2 { get; set; }
+		public string? Description2 { get; set; }
         public bool Status { get; set; }
     }
 }
